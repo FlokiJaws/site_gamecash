efd75639e151f623abf58f0ffba5e77c93db95d2
FlokiJaws <95760524+FlokiJaws@users.noreply.github.com>
2025-04-07 14:51:36 +0200

    changement

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Gaming from "./pages/Gaming";
-import Retro from "./pages/Retro";
-import TCG from "./pages/TCG";
-import Goodies from "./pages/Goodies";
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <div className="container">
-      <header>
-        <img src="/gamecash_logo.png" alt="Gamecash Logo" className="logo" />
-        <h1 className="title">Gamecash</h1>
+    <div className="app">
+      <header className="logo-container">
+        <img src="/gamecash_logo.png" alt="Gamecash logo" className="logo" />
       </header>
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <main className="choices">
-              <Link to="/gaming" className="choice-btn">Gaming</Link>
-              <Link to="/retro" className="choice-btn">Retro</Link>
-              <Link to="/tcg" className="choice-btn">TCG</Link>
-              <Link to="/goodies" className="choice-btn">Goodies - Culture Geek</Link>
-            </main>
-          }
-        />
-        <Route path="/gaming" element={<Gaming />} />
-        <Route path="/retro" element={<Retro />} />
-        <Route path="/tcg" element={<TCG />} />
-        <Route path="/goodies" element={<Goodies />} />
-      </Routes>
+      <main className="grid-container">
+        <div className="grid-button" onClick={() => {}}>🎮 Jeux Vidéo</div>
+        <div className="grid-button" onClick={() => {}}>🛒 Vendre un Jeu</div>
+        <div className="grid-button" onClick={() => {}}>📦 Produits Dérivés</div>
+        <div className="grid-button" onClick={() => {}}>👤 Espace Client</div>
+      </main>
     </div>
   );
 }
+
+export default App;
